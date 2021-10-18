6824c4cc90bbba3e90254834d9200ad91655a7bc
Jafeth Zuniga <87389094+jzuniga10@users.noreply.github.com>
2021-10-18 15:07:47 -0500

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,10 +1,6 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.15.0"
-    }
-  }
+provider "docker" {
+  version = "~> 2.15.0"
+  host    = "unix:///var/run/docker.sock"
 }
 
 module "label" {
